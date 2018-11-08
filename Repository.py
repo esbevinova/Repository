b8688d06b3f612d9d53ab9c6f029785b7fb7b631
esbevinova <44759211+esbevinova@users.noreply.github.com>
2018-11-08 07:49:37 -0500

    added hw10 features

--- Repository.py
+++ Repository.py
@@ -1,6 +1,6 @@
 """SSW-810-A
    Ekaterina (katya) Bevinova
-   HW09
+   HW10
 """
 from prettytable import PrettyTable
 from HW08ebevinova import read_file
@@ -9,10 +9,9 @@ import os
 from collections import defaultdict
 
 
-
 class Student:
     "A class that stores all the information about students."
-    pt_hdr = ['CWID', 'Name', 'Completed Courses']
+    pt_hdr = ['CWID', 'Name', 'Major', 'Completed Courses', 'Remaining Required', 'Remaining Electives']
     def __init__(self, cwid, name, major):
         self.cwid = cwid
         self.name = name
@@ -24,9 +23,46 @@ class Student:
         """A function that assigns a value grade to the key course."""
         self.courses[course] = grade
     
-    def pt_row(self):
+    def pt_row(self, maj):
         """"A function that creates rows with student's information."""
-        return [self.cwid, self.name, sorted(self.courses.keys())]
+        completed_courses, remaining_required, remaining_electives = maj.calculated_courses(self.courses)
+        return [self.cwid, self.name, self.major, completed_courses, remaining_required, remaining_electives]
+    
+
+class Majors:
+    "A class that stores all the information about required and elective courses."
+    pt_hdr = ['Dept', 'Required', 'Electives']
+    def __init__(self, dept):
+        self.dept = dept
+        self.required = set()
+        self.electives = set()
+        
+    
+    def add(self, flag, course):
+        if flag == 'R':
+            self.required.add(course)
+        elif flag == 'E':
+            self.electives.add(course)     
+    
+    def calculated_courses(self, courses):
+        """Return completed_courses, remaining_required, remaining_electives where courses is dict with key as a course and value as a grade"""
+        remaining_required = self.required
+        remaining_electives = self.electives
+        completed_courses = set()
+
+        for course, grade in courses.items():
+            if grade in ('A', 'A-', 'B+', 'B-', 'B', 'C', 'C-', 'C+'):
+                completed_courses.add(course)
+            
+        remaining_required = self.required - completed_courses
+            
+        if self.electives.intersection(completed_courses):
+            remaining_electives = None
+            
+        return completed_courses, remaining_required, remaining_electives
+    
+    def pt_row(self):
+        return [self.dept, self.required, self.electives]
     
 
 class Instructor:
@@ -54,10 +90,12 @@ class Repository:
         self.students = dict() #cwid is the key, Instance of class Student is the value
         self.instructors = dict() #cwid is they, Instance of class Instructor is the value
         self.grades = list()
+        self.majors = dict() #major is the key, Instance of class Majors is the value
 
         self.reading_students(os.path.join(path, 'students.txt'))
         self.get_instructors(os.path.join(path, 'instructors.txt'))
         self.get_grades(os.path.join(path, 'grades.txt'))
+        self.reading_majors(os.path.join(path, 'majors.txt'))
 
         if ptables:
             print("\nStudent Summary")
@@ -66,11 +104,20 @@ class Repository:
             print("\nInstructor Summary")
             self.instructor_table()
 
+            print("\nMajors Summary")
+            self.majors_table()
+
+    def majors_table(self):
+        pt = PrettyTable(field_names = Majors.pt_hdr)
+        for dept in self.majors.values():
+            pt.add_row(dept.pt_row())
+        print (pt)
+
     def student_table(self):
         """A function that creates a table with student's information."""
         pt = PrettyTable(field_names = Student.pt_hdr)
         for student in self.students.values():
-            pt.add_row(student.pt_row())
+            pt.add_row(student.pt_row(self.majors[student.major]))
         print (pt)
     
     def instructor_table(self):
@@ -80,6 +127,15 @@ class Repository:
             for row in instructor.pt_row():
                 pt.add_row(row) 
         print (pt)
+
+    def reading_majors(self, path):
+        try:
+            for dept, flag, course in read_file(path, 3, '\t', header=False):
+                if dept not in self.majors: #if dept is not in majors (dict) then we add a new instance of it
+                    self.majors[dept] = Majors(dept)
+                self.majors[dept].add(flag, course) #add flag, course to the major through every line
+        except ValueError as e:
+            print (e)
 
     def reading_students(self, path):
         """A function that assigns student's information (cwid, name, major) to his/her cwid."""
